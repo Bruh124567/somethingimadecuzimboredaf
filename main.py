28ddde56217b284ea6da598005ae314ccaba7d52
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-05 10:18:31 +0000

    Initial version(gui coming soon)

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+def main():
+    print ("Choose an option:")
+    print ("1. Start Game")
+    print ("2. Quit Game")
+    option = input("Enter your option: ")
+    if option == "1":
+        start_game()
+    elif option == "2":
+        print ("Goodbye!")
+        exit()
+
+def start_game():
+    number = random.randint(1, 50)
+    print ("Welcome to the game!")
+    print ("I'm thinking of a number between 1 and 50.")
+    guess = input("Guess the number: ")
+    if guess.isdigit() and 1 <= int(guess) <= 50:
+        if int(guess) == number:
+            print ("Congratulations, you won!")
+            play_again()
+        else:
+            print ("Sorry, that's not the number I was thinking of.")
+            print ("The number I was thinking of was", number)
+            play_again()
+            
+def play_again():
+    print ("Would you like to play again?")
+    print ("1. Yes")
+    print ("2. No")
+    wyltpa = input("Enter your choice: ")
+    if wyltpa == "1":
+        start_game()
+    else:
+        print ("Goodbye!")
+        exit()
+main()
