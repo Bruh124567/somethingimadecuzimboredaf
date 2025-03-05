156f7bc9edecb734efebe8211cd8ddb3fa621ed2
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-05 12:10:11 +0000

    BROKEN VERSION DON'T USE

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random # used for generating the random number
-
+difficulty = 0
 def main():
     print ("Choose an option:")
     print ("1. Start Game")
@@ -18,21 +18,17 @@ def start_game():
     print("2. Medium")
     print("3. Hard")
     print("4. Insane")
-    difficulty = input("Enter your difficulty: ")
-    if difficulty == "1":
-        game_diff1()
-    elif difficulty == "2":
-        game_diff2()
-    elif difficulty == "3":
-        game_diff3()
-    elif difficulty == "4":
-        game_diff4()
+    print ("5. Custom")
+    difficulty = input("Enter your choice: ")
+    if difficulty.isdigit() and 1 <= int(difficulty) <= 4:
+        game()
+    elif difficulty == 5:
+        set_custom_difficulty()
     else:
-        print("Invalid difficulty")
+        print("Invalid difficulty!")
         start_game()
 
-
-def game_diff1():
+def game():
     number = random.randint(1, 10)
     print ("Easy")
     print ("I'm thinking of a number between 1 and 10.")
@@ -45,49 +41,9 @@ def game_diff1():
             print ("Sorry, that's not the number I was thinking of.")
             print ("The number I was thinking of was", number)
             play_again()
-
-def game_diff2():
-    number = random.randint(1, 20)
-    print ("Medium")
-    print ("I'm thinking of a number between 1 and 20.")
-    guess = input("Guess the number: ")
-    if guess.isdigit() and 1 <= int(guess) <= 20: # checks if the variable guess is an integer and if that is between 1 and 20
-        if int(guess) == number:
-            print ("Congratulations, you won!")
-            play_again()
-        else:
-            print ("Sorry, that's not the number I was thinking of.")
-            print ("The number I was thinking of was", number)
-            play_again()
-
-def game_diff3():
-    number = random.randint(1, 30)
-    print ("Hard")
-    print ("I'm thinking of a number between 1 and 30.")
-    guess = input("Guess the number: ")
-    if guess.isdigit() and 1 <= int(guess) <= 30: # checks if the variable guess is an integer and if that is between 1 and 30
-        if int(guess) == number:
-            print ("Congratulations, you won!")
-            play_again()
-        else:
-            print ("Sorry, that's not the number I was thinking of.")
-            print ("The number I was thinking of was", number)
-            play_again()
-
-def game_diff4():
-    number = random.randint(1, 50)
-    print ("Insane")
-    print ("I'm thinking of a number between 1 and 50.")
-    guess = input("Guess the number: ")
-    if guess.isdigit() and 1 <= int(guess) <= 50: # checks if the variable guess is an integer and if that is between 1 and 50
-        if int(guess) == number:
-            print ("Congratulations, you won!")
-            play_again()
-        else:
-            print ("Sorry, that's not the number I was thinking of.")
-            print ("The number I was thinking of was", number)
-            play_again()
-
+def set_custom_difficulty():
+    print("Coming soon!")
+    start_game()
 def play_again():
     print ("Would you like to play again?")
     print ("1. Yes")
