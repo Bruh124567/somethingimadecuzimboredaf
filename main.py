2fca32e78a97cfe8662ca77e0ad2c777493c44eb
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-05 16:49:11 +0000

    cleaned up code and added teaser for custom difficulty

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def main():
     print ("2. Quit Game")
     option = input("Enter your option: ")
     if option == "1":
-        start_game()
+        game()
     elif option == "2":
         print ("Goodbye!")
         exit()
 
-def start_game():
+
+def game():
     print ("IMPORTANT: Only the first difficulty is implemented for now!!!!")
     print("Choose a difficulty:")
     print("1. Easy")
@@ -20,20 +21,27 @@ def start_game():
     print("4. Insane")
     print ("5. Custom")
     difficulty = input("Enter your choice: ")
-    if difficulty.isdigit() and 1 <= int(difficulty) <= 4:
-        game()
-    elif difficulty == 5:
-        set_custom_difficulty()
+    if difficulty == "1":
+        number = random.randint(1, 10)
+        print ("Easy")
+        print ("I'm thinking of a number between 1 and 10.")
+    elif difficulty == "2":
+        number = random.randint(1, 20)
+        print ("Medium")
+        print ("I'm thinking of a number between 1 and 20.")
+    elif difficulty == "3":
+        number = random.randint(1, 30)
+        print ("Hard")
+        print ("I'm thinking of a number between 1 and 30.")
+    elif difficulty == "4":
+        number = random.randint(1, 50)
+        print ("Insane")
+        print ("I'm thinking of a number between 1 and 50.")
     else:
-        print("Invalid difficulty!")
-        start_game()
-
-def game():
-    number = random.randint(1, 10)
-    print ("Easy")
-    print ("I'm thinking of a number between 1 and 10.")
+        print("An error occoured. Restarting the game...")
+        game()
     guess = input("Guess the number: ")
-    if guess.isdigit() and 1 <= int(guess) <= 10: # checks if the variable guess is an integer and if that is between 1 and 10
+    if guess.isdigit() and 1 <= int(guess) <= 50: # checks if the variable guess is an integer and if it's between 1 and 50
         if int(guess) == number:
             print ("Congratulations, you won!")
             play_again()
@@ -41,16 +49,18 @@ def game():
             print ("Sorry, that's not the number I was thinking of.")
             print ("The number I was thinking of was", number)
             play_again()
+
 def set_custom_difficulty():
     print("Coming soon!")
-    start_game()
+    game()
+
 def play_again():
     print ("Would you like to play again?")
     print ("1. Yes")
     print ("2. No")
     wyltpa = input("Enter your choice: ")
     if wyltpa == "1":
-        start_game()
+        game()
     else:
         print ("Goodbye!")
         exit()
