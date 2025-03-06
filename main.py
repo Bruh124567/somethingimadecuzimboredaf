c52a0eeaaf54af364938402d90eaf832877ce3d7
Bruh124567 <81039620+Bruh124567@users.noreply.github.com>
2025-03-06 08:25:13 +0000

    "Re"implemented the difficulties and fixed some stuff i guess

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 
 
 def game():
-    print ("IMPORTANT: Only the first difficulty is implemented for now!!!!")
+    print ("I might have broken this thing but idc.")
     print("Choose a difficulty:")
     print("1. Easy")
     print("2. Medium")
